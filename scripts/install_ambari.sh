dd360a807ae266d6ac027f7d25050e84b7e76cee
ClassCat <support@classcat.com>
2015-10-17 13:43:34 +0900

    ambari

---
+++ scripts/install_ambari.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt-get install ntp
+
+cd /etc/apt/sources.list.d
+
+wget http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.1.2/ambari.list
+
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+
+apt-get update
+
+apt-get install ambari-server
+
+# ambari-server setup
+
+# ambari-server start
+
+
+cd ~
+
+ssh-keygen -t rsa -P ""
+
+
