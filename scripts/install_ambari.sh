5aa97e37d4b7ae1aa6ac9e8022b0e7299b39327b
ClassCat <support@classcat.com>
2015-10-17 14:25:15 +0900

    ambari

--- scripts/install_ambari.sh
+++ scripts/install_ambari.sh
@@ -10,7 +10,7 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
 
 apt-get update
 
-apt-get install ambari-server
+apt-get install -y ambari-server
 
 # ambari-server setup
 
