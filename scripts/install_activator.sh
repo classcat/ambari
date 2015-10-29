36286139d1b9be1f069ec2f78644edee87a3eb49
ClassCat <support@classcat.com>
2015-10-29 23:36:55 +0900

    activator

---
+++ scripts/install_activator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###############################################################
+# Copyright (C) 2015 ClassCat(R) Co.,Ltd. All rights reserved.
+###############################################################
+
+# --- HISOTRY -------------------------------------------------
+#
+# -------------------------------------------------------------
+
+export LC_ALL=C
+
+mkdir -p /opt/packages
+
+cd /opt/packages
+
+wget https://downloads.typesafe.com/typesafe-activator/1.3.6/typesafe-activator-1.3.6-minimal.zip
+# wget https://downloads.typesafe.com/typesafe-activator/1.3.6/typesafe-activator-1.3.6-minimal.zip?_ga=1.66975274.56746480.1445890795
+
+apt-get update
+
+apt-get install -y unzip
+
+unzip typesafe-activator-1.3.6-minimal.zip
+
+ln -s /opt/packages/activator-1.3.6-minimal /opt/activator
+
+ln -s /opt/activator/activator /usr/local/activator
