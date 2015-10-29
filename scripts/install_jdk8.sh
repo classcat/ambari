37f5dc965ec52e8977751217fbe134619da1a946
ClassCat <support@classcat.com>
2015-10-29 23:54:57 +0900

    spark, jdk

--- scripts/install_jdk8.sh
+++ scripts/install_jdk8.sh
@@ -5,6 +5,7 @@
 ###############################################################
 
 #--- HISTORY ----------------------------------------------------------
+# 29-oct-15 : jdk8u65
 # 17-oct-15 : discard jdk.sh, instead, use update-alternatives.
 # 15-oct-15 : change the path.
 # 15-oct-15 : fixed.
@@ -13,19 +14,24 @@
 
 export LC_ALL=C
 
+TARGET=jdk1.8.0_65
+
 CWD=`pwd`
 
 mkdir -p /opt/packages
 
 cd /opt/packages
 
-wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
+wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u65-b17/jdk-8u65-linux-x64.tar.gz
+#wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
 
-tar xfz jdk-8u5-linux-x64.tar.gz
+tar xfz jdk-8u65-linux-x64.tar.gz
+#tar xfz jdk-8u5-linux-x64.tar.gz
 
 mkdir -p /usr/lib/jvm
 
-mv jdk1.8.0_05 /usr/lib/jvm
+mv $TARGET /usr/lib/jvm
+#mv jdk1.8.0_05 /usr/lib/jvm
 
 #cat << _EOB_ > /etc/profile.d/jdk.sh
 #export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_05
@@ -33,10 +39,12 @@ mv jdk1.8.0_05 /usr/lib/jvm
 ##export PATH=\$PATH:\$JAVA_HOME/bin
 #_EOB_
 
+update-alternatives --install "/usr/bin/java"   "java"   "/usr/lib/jvm/${TARGET}/bin/java"   1
+update-alternatives --install "/usr/bin/javac"  "javac"  "/usr/lib/jvm/${TARGET}/bin/javac"  1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/${TARGET}/bin/javaws" 1
 
-update-alternatives --install "/usr/bin/java"   "java"   "/usr/lib/jvm/jdk1.8.0_05/bin/java"   1
-update-alternatives --install "/usr/bin/javac"  "javac"  "/usr/lib/jvm/jdk1.8.0_05/bin/javac"  1
-update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_05/bin/javaws" 1
-
+#update-alternatives --install "/usr/bin/java"   "java"   "/usr/lib/jvm/jdk1.8.0_05/bin/java"   1
+#update-alternatives --install "/usr/bin/javac"  "javac"  "/usr/lib/jvm/jdk1.8.0_05/bin/javac"  1
+#update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_05/bin/javaws" 1
 
 exit 0
---
+++ scripts/install_spark_wo_account.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###############################################################
+# Copyright (C) 2015 ClassCat(R) Co.,Ltd. All rights reserved.
+###############################################################
+
+# --- HISOTRY -------------------------------------------------
+
+# -------------------------------------------------------------
+
+export LC_ALL=C
+
+
+# install spark package
+mkdir -p /opt/packages
+
+cd /opt/packages
+
+wget http://ftp.riken.jp/net/apache/spark/spark-1.5.1/spark-1.5.1-bin-hadoop2.4.tgz
+
+tar xfz spark-1.5.1-bin-hadoop2.4.tgz
+
+ln -s /opt/packages/spark-1.5.1-bin-hadoop2.4 /opt/spark
+
+cp -p /opt/spark/conf/log4j.properties.template /opt/spark/conf/log4j.properties
+
+sed -i.bak -e "s/^log4j\.rootCategory\s*=\s*INFO.*/log4j.rootCategory=WARN, console/" /opt/spark/conf/log4j.properties
+
+echo 'export PATH=/opt/spark/bin:$PATH' > /home/ubuntu/.bash_profile
+chown ubuntu:ubuntu /home/spark/.bash_profile
+
+exit 0
